dfbaf8d61c49cf51ce36e64808871b20f755fa0b
Luiz Ricardo <64525544+rickluizms@users.noreply.github.com>
2023-04-11 22:33:12 -0300

    Add files via upload

---
+++ imdb250/imdb250/spiders/imdb.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb'
+    start_urls = ['https://www.imdb.com/chart/top/']
+
+    def parse(self, response):
+        for filmes in response.css('.titleColumn'):
+            yield {
+                'titulos': filmes.css('.titleColumn a::text').get(),
+                'ano': filmes.css('.secondaryInfo::text').get()[1:-1],
+                'nota': response.css('strong::text').get()
+            }
+
